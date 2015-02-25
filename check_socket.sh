7acd0a099d9c2e62969bb5d4e0781d9e9ca8d482
Romain Aviolat <aviolat@ned-team.com>
2015-02-25 08:18:01 +0100

    initial commit

---
+++ check_socket.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# GENERATED FROM PUPPET
+###########
+# 2015.02.25 rav@rndoi - initial file creation
+# check if a socket exists for a given address/port
+
+
+tcp_opt=""
+udp_opt=""
+nb_sockets=0
+
+help="Usage: $0 -a <listen address> -p <port> -t (tcp) -u (udp)"
+
+while getopts 'a:p:tu' opt; do
+  case $opt in
+    a)
+      address="${OPTARG}"
+      ;;
+    p)
+      port="${OPTARG}"
+      ;;
+    t)
+      tcp_netstat_opt="t"
+      tcp_text="TCP"
+      ;;
+    u)
+      udp_netstat_opt="u"
+      udp_text="UDP"
+      ;;
+    \?|:)
+      echo ${help}
+      exit 3
+      ;;
+  esac
+done
+
+# standard Icinga plugin return codes.
+STATUS_OK=0
+STATUS_WARNING=1
+STATUS_CRITICAL=2
+STATUS_UNKNOWN=3
+
+# control the options passed to the script
+if [ -n "$tcp_netstat_opt" ] ; then
+  let "nb_sockets = $nb_sockets + 1"
+fi
+
+if [ -n "$udp_netstat_opt" ] ; then
+  let "nb_sockets = $nb_sockets + 1"
+fi
+
+if [ -z ${address} ] || [ -z ${port} ] || [ $nb_sockets == 0 ]  ; then
+  echo ${help}
+  exit 3
+fi
+
+output=$(netstat -"$tcp_netstat_opt""$udp_netstat_opt"ln4 | grep "$address:$port" | wc -l)
+
+# formated output
+if [ "$nb_sockets" == "$output" ] ; then
+  echo "OK - listening on "$address":"$port" "$tcp_text" "$udp_text""
+
+else
+  echo "WARNING - not listening on "$address":"$port" "$tcp_text" "$udp_text""
+
+fi
+
